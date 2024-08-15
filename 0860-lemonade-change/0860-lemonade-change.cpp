4f63d84777463d72ca8fe647172d03d6caa7afa6
kinantanbagaspati <63950453+kinantanbagaspati@users.noreply.github.com>
2024-08-15 13:40:47 +0700

    Time: 84 ms (11.23%), Space: 86.1 MB (32.71%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.cpp
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        unsigned short freq[2] = {0, 0}, n = bills.size();
-        for(int i=0; i<n; i++){
-            if(bills[i] == 5){
+        unsigned short freq[2] = {0, 0};
+        for(auto bill : bills){
+            if(bill == 5){
                 freq[0]++;
-            }else if(bills[i] == 10){
+            }else if(bill == 10){
                 if(!freq[0]) return false;
                 freq[0]--; freq[1]++;
             }else{
